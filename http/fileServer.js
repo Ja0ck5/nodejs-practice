e6a9cb798373a3efe0ade8716f97fa4120582f6f
Ja0ck5 <975117619@qq.com>
2016-12-29 16:14:00 +0800

    file server

--- http/fileServer.js
+++ http/fileServer.js
@@ -1,2 +1,74 @@
-console.log('test');
-console.log('test');
+'use strict';
+//解析URL需要用到Node.js提供的url模块，它使用起来非常简单，通过parse()将一个字符串解析为一个Url对象
+var url = require('url');
+
+console.log(url.parse('http://user:pass@host.com:8080/path/to/file?query=string#hash'));
+/**
+ * Url {
+  protocol: 'http:',
+  slashes: true,
+  auth: 'user:pass',
+  host: 'host.com:8080',
+  port: '8080',
+  hostname: 'host.com',
+  hash: '#hash',
+  search: '?query=string',
+  query: 'query=string',
+  pathname: '/path/to/file',
+  path: '/path/to/file?query=string',
+  href: 'http://user:pass@host.com:8080/path/to/file?query=string#hash' }
+ */
+
+// 处理本地文件目录需要使用Node.js提供的path模块，它可以方便地构造目录
+var path = require('path');
+//path:[object Object]
+console.log('path:'+path);
+// 解析当前目录:
+var workDir = path.resolve('.');
+//c:\Users\Ja0ck5\Desktop\data\nodejs
+console.log('workDir:'+workDir);
+// 组合完整的文件路径:当前目录+'pub'+'index.html':
+var filePath = path.join(workDir, 'pub', 'index.html');
+console.log('filePath:'+filePath);
+// 'c:\Users\Ja0ck5\Desktop\data\nodejs\pub\index.html'
+
+//////////////////////////////////////////////////////////////////// server  //////////////////////////////////////////////////////////////////
+
+var
+    fs = require('fs'),
+    url = require('url'),
+    path = require('path'),
+    http = require('http');
+
+// 从命令行参数获取root目录，默认是当前目录:
+var root = path.resolve(process.argv[2] || '.');
+
+console.log('Static root dir: ' + root);
+
+// 创建服务器:
+var server = http.createServer(function (request, response) {
+    // 获得URL的path，类似 '/css/bootstrap.css':
+    var pathname = url.parse(request.url).pathname;
+    // 获得对应的本地文件路径，类似 '/srv/www/css/bootstrap.css':
+    var filepath = path.join(root, pathname);
+    // 获取文件状态:
+    fs.stat(filepath, function (err, stats) {
+        if (!err && stats.isFile()) {
+            // 没有出错并且文件存在:
+            console.log('200 ' + request.url);
+            // 发送200响应:
+            response.writeHead(200);
+            // 将文件流导向response:
+            fs.createReadStream(filepath).pipe(response);
+        } else {
+            // 出错了或者文件不存在:
+            console.log('404 ' + request.url);
+            // 发送404响应:
+            response.writeHead(404);
+            response.end('404 Not Found');
+        }
+    });
+}).listen(80);
+
+console.log('Server is running at http://127.0.0.1:80/');
+//////////////////////////////////////////////////////////////////// server  //////////////////////////////////////////////////////////////////
