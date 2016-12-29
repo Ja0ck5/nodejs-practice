4e3a5bd81c011bfa6378586eb0a85d020f44750c
Ja0ck5 <975117619@qq.com>
2016-12-29 14:57:26 +0800

    疑问 默认关闭 wirtable 流

--- 01/main.js
+++ 01/main.js
@@ -121,5 +121,7 @@ var ws = fs.createWriteStream('01/copied.txt');
 
 rs.pipe(ws);
 
+//ws.write('haha-test-is-closed');
+
 //默认情况下，当Readable流的数据读取完毕，end事件触发后，将自动关闭Writable流。如果我们不希望自动关闭Writable流，需要传入参数：
 //readable.pipe(writable, { end: false });
