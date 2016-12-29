8e25c933900253653800bf86173d270bb12253cd
Ja0ck5 <975117619@qq.com>
2016-12-29 14:51:28 +0800

    不自动关闭 wirtable 流

--- 01/main.js
+++ 01/main.js
@@ -120,3 +120,6 @@ var rs = fs.createReadStream('01/sample.txt');
 var ws = fs.createWriteStream('01/copied.txt');
 
 rs.pipe(ws);
+
+//默认情况下，当Readable流的数据读取完毕，end事件触发后，将自动关闭Writable流。如果我们不希望自动关闭Writable流，需要传入参数：
+//readable.pipe(writable, { end: false });
