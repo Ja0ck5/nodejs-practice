fb07f7b97c098d90dd135154c934174e8a52b365
Ja0ck5 <975117619@qq.com>
2016-12-30 11:39:19 +0800

    add bodyParser

--- hello-koa/hellokoa.js
+++ hello-koa/hellokoa.js
@@ -34,6 +34,7 @@ const Koa = require('koa');
 
 // 注意require('koa-router')返回的是函数:
 const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 
@@ -49,10 +50,34 @@ router.get('/hello/:name', async (ctx, next) => {
     ctx.response.body = `<h1>Hello, ${name}!</h1>`;
 });
 
+// router.get('/', async (ctx, next) => {
+//     ctx.response.body = '<h1>Index</h1>';
+// });
+
 router.get('/', async (ctx, next) => {
-    ctx.response.body = '<h1>Index</h1>';
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
 });
 
+router.post('/signin', async (ctx, next) => {
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+});
+
+//add bodyParser middleware must before router
+app.use(bodyParser());
 // add router middleware:
 app.use(router.routes());
 
