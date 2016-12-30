c3422789e2233d861505bd54237fd3e5fe9c265d
Ja0ck5 <975117619@qq.com>
2016-12-30 10:54:46 +0800

    start hellokoa

--- hello-koa/hellokoa.js
+++ hello-koa/hellokoa.js
@@ -9,8 +9,5 @@ app.use(async (ctx, next) => {
     await next();
     ctx.response.type = 'text/html';
     ctx.response.body = '<h1>Hello, koa2!</h1>';
-});
-
-// 在端口3000监听:
-app.listen(3000);
-console.log('app started at port 3000...');
+}).listen(80);
+console.log('app started at port 80...');
