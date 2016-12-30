7dac84cba888ab67cc32e22301e4aa43727726e8
Ja0ck5 <975117619@qq.com>
2016-12-30 11:09:24 +0800

    test async middleware

--- hello-koa/hellokoa.js
+++ hello-koa/hellokoa.js
@@ -12,3 +12,21 @@ app.use(async (ctx,next)=>{
     ctx.response.type='text/html';
     ctx.response.body = '<h1>Hello Koa2</h1>';
 }).listen(80);
+
+app.use(async (ctx, next) => {
+    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
+    await next(); // 调用下一个middleware
+});
+
+app.use(async (ctx, next) => {
+    const start = new Date().getTime(); // 当前时间
+    await next(); // 调用下一个middleware
+    const ms = new Date().getTime() - start; // 耗费时间
+    console.log(`Time: ${ms}ms`); // 打印耗费时间
+});
+
+app.use(async (ctx, next) => {
+    await next();
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>Hello, koa2!</h1>';
+});
