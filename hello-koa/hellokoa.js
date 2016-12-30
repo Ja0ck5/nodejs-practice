083be35521ccb84eb14307b44174ec6d3a73c476
Ja0ck5 <975117619@qq.com>
2016-12-30 10:57:52 +0800

    test helloKoa

--- hello-koa/hellokoa.js
+++ hello-koa/hellokoa.js
@@ -5,9 +5,10 @@ const Koa = require('koa');
 const app = new Koa();
 
 // 对于任何请求，app将调用该异步函数处理请求：
-app.use(async (ctx, next) => {
-    await next();
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
-}).listen(80);
 console.log('app started at port 80...');
+
+app.use(async (ctx,next)=>{
+    await next();
+    ctx.response.type='text/html';
+    ctx.response.body = '<h1>Hello Koa2</h1>';
+}).listen(80);
