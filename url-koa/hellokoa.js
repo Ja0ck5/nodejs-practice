d1b97a90fccdb495d2b8ecb1138fde93eb557a53
Ja0ck5 <975117619@qq.com>
2016-12-30 15:56:24 +0800

    edit controller js

--- url-koa/hellokoa.js
+++ url-koa/hellokoa.js
@@ -1,40 +1,8 @@
-// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
 
-// // 创建一个Koa对象表示web app本身:
-// const app = new Koa();
-
-// // 对于任何请求，app将调用该异步函数处理请求：
-// console.log('app started at port 80...');
-
-// app.use(async (ctx,next)=>{
-//     await next();
-//     ctx.response.type='text/html';
-//     ctx.response.body = '<h1>Hello Koa2</h1>';
-// }).listen(80);
-
-// app.use(async (ctx, next) => {
-//     console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
-//     await next(); // 调用下一个middleware
-// });
-
-// app.use(async (ctx, next) => {
-//     const start = new Date().getTime(); // 当前时间
-//     await next(); // 调用下一个middleware
-//     const ms = new Date().getTime() - start; // 耗费时间
-//     console.log(`Time: ${ms}ms`); // 打印耗费时间
-// });
-
-// app.use(async (ctx, next) => {
-//     await next();
-//     ctx.response.type = 'text/html';
-//     ctx.response.body = '<h1>Hello, koa2!</h1>';
-// });
-
-
-// 注意require('koa-router')返回的是函数:
-const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
+
+const controller = require('./controller');
 
 const app = new Koa();
 
@@ -44,42 +12,11 @@ app.use(async (ctx, next) => {
     await next();
 });
 
-// add url-route:
-router.get('/hello/:name', async (ctx, next) => {
-    var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-});
-
-// router.get('/', async (ctx, next) => {
-//     ctx.response.body = '<h1>Index</h1>';
-// });
-
-router.get('/', async (ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>
-        <form action="/signin" method="post">
-            <p>Name: <input name="name" value="koa"></p>
-            <p>Password: <input name="password" type="password"></p>
-            <p><input type="submit" value="Submit"></p>
-        </form>`;
-});
-
-router.post('/signin', async (ctx, next) => {
-    var
-        name = ctx.request.body.name || '',
-        password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-    } else {
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
-    }
-});
-
-//add bodyParser middleware must before router
+// parse request body:
 app.use(bodyParser());
-// add router middleware:
-app.use(router.routes());
+
+// add controllers:
+app.use(controller());
 
 app.listen(3000);
 console.log('app started at port 3000...');
