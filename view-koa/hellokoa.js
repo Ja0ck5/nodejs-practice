ffd57d24567e45350b6583ad41a5008bb3841af3
Ja0ck5 <975117619@qq.com>
2016-12-31 14:45:56 +0800

    add controller

--- view-koa/hellokoa.js
+++ view-koa/hellokoa.js
@@ -6,6 +6,13 @@ const controller = require('./controller');
 
 const app = new Koa();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+app.use(templating('view', {
+    noCache: !isProduction,
+    watch: !isProduction
+}));
+
 // log request URL:
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
@@ -18,5 +25,9 @@ app.use(bodyParser());
 // add controllers:
 app.use(controller());
 
+let staticFiles = require('./static-files');
+app.use(staticFiles('/static/', __dirname + '/static'));
+
 app.listen(3000);
 console.log('app started at port 3000...');
+
