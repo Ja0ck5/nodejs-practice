d7367341417a0778249fe37fa441355da483851b
Ja0ck5 <975117619@qq.com>
2016-12-31 15:00:23 +0800

    add launch js

--- view-koa/hellokoa.js
+++ view-koa/hellokoa.js
@@ -4,30 +4,40 @@ const bodyParser = require('koa-bodyparser');
 
 const controller = require('./controller');
 
+const templating = require('./templating');
+
 const app = new Koa();
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-app.use(templating('view', {
-    noCache: !isProduction,
-    watch: !isProduction
-}));
-
 // log request URL:
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    var
+        start = new Date().getTime(),
+        execTime;
     await next();
+    execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
+
+// static file support:
+if (! isProduction) {
+    let staticFiles = require('./static-files');
+    app.use(staticFiles('/static/', __dirname + '/static'));
+}
 
 // parse request body:
 app.use(bodyParser());
 
-// add controllers:
-app.use(controller());
+// add nunjucks as view:
+app.use(templating('views', {
+    noCache: !isProduction,
+    watch: !isProduction
+}));
 
-let staticFiles = require('./static-files');
-app.use(staticFiles('/static/', __dirname + '/static'));
+// add controller:
+app.use(controller());
 
 app.listen(3000);
 console.log('app started at port 3000...');
-
